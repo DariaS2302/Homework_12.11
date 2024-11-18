385af5fac3ca62b95f3cff0e4e725373fc1440a9
Sarycheva Daria <d.saricheva@gmail.com>
2024-11-18 15:51:57 +0300

    add EditTestBase

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -6,17 +6,18 @@ import io.restassured.RestAssured;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 
+
 public class TestBase {
 
     @BeforeAll
     public static void setUp() {
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("browserVersion", "100.0");
+        Configuration.browserSize = System.getProperty("browserSize");
         RestAssured.baseURI = "https://demoqa.com";
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.pageLoadStrategy = "eager";
-        Configuration.browserSize = System.getProperty("browserSize");
         Configuration.remote = System.getProperty("remoteURL");
-        Configuration.browser = System.getProperty("browser", "chrome");
-        Configuration.browserVersion = System.getProperty("browserVersion");
     }
 
     @AfterEach
