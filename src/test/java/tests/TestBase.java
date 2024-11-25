939b0a1bc4fbe670e1cef1bf625fd1d5b01fb084
Sarycheva Daria <d.saricheva@gmail.com>
2024-11-25 11:30:45 +0300

    add EditTestBase

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,7 +1,6 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
-import com.codeborne.selenide.Selenide;
 import com.codeborne.selenide.logevents.SelenideLogger;
 import helpers.Attach;
 import io.qameta.allure.selenide.AllureSelenide;
@@ -14,7 +13,7 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import java.util.Map;
 
 import static com.codeborne.selenide.Selenide.closeWebDriver;
-import static java.lang.String.format;
+
 
 
 public class TestBase {
@@ -23,24 +22,21 @@ public class TestBase {
     public static void setUp() {
 
         RestAssured.baseURI = "https://demoqa.com";
-
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.pageLoadStrategy = "eager";
+        Configuration.browser = System.getProperty("browser", "chrome");
+        Configuration.browserVersion = System.getProperty("browserVersion", "125.0");
+        Configuration.browserSize = System.getProperty("browserSize");
+        Configuration.remote = System.getProperty("remoteURL");
 
         DesiredCapabilities capabilities = new DesiredCapabilities();
         capabilities.setCapability("selenoid:options", Map.<String, Object>of(
                 "enableVNC", true,
                 "enableVideo", true
         ));
-
-        Configuration.pageLoadStrategy = "eager";
         Configuration.browserCapabilities = capabilities;
 
-        String login = System.getProperty("login");
-        String rwhost = System.getProperty("rwhost");
-        if (login != null && rwhost != null) Configuration.remote = format("https://%s@%s/wd/hub", login, rwhost);
-        Configuration.baseUrl = "https://demoqa.com";
-        Configuration.browser = System.getProperty("browser", "chrome");
-        Configuration.browserVersion = System.getProperty("browserVersion", "125.0");
-        Configuration.browserSize = System.getProperty("browserSize", "1920x1080");
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
     }
 
     @BeforeEach
